6989adc47735a90f5260cf0e178251f7e6883b14
absanyal <absanyal.ABS@gmail.com>
2024-02-08 15:58:27 -0500

    basic submit script added

---
+++ submit_pc.sh
@@ -0,0 +1,27 @@
+mkdir -p runs
+cd runs
+
+for i in {1..10}
+do
+echo "submitting job $i"
+    
+mkdir -p run_$i
+cd run_$i
+
+cp ../../polymer.py .
+cp ../../link_dist.py .
+cp ../../cylindermath.py .
+cp ../../angle.py .
+
+mkdir -p data
+mkdir -p plots
+
+python3 polymer.py
+# time mpirun -n 8 lmp_mpi -i input.lammps > out.run
+# python3 link_dist.py
+
+echo "--------------------------------"
+
+cd ..
+
+done
