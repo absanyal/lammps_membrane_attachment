fc5961248b39d0969df16e9d8e95a1457aa5416c
absanyal <absanyal.ABS@gmail.com>
2024-03-14 12:56:20 -0400

    Update D_rot value output in D_rot_sampled.py

--- D_rot_sampled.py
+++ D_rot_sampled.py
@@ -40,6 +40,7 @@ with open('data/D_rot_sampled.txt', 'w') as file:
 fit_params, fit_cov = curve_fit(f, t_shortened, ang_dev_sq_avg_sampled)
 print('Fitted parameters: m = {:.4e}, c = {:.4e}'.format(fit_params[0], fit_params[1]))
 print('Diffusion coefficient: D = {:.4e}'.format(fit_params[0]/(2)))
+print('or, approximately D = {:.4f}'.format(fit_params[0]/(2)))
 
 fitline = f(t_shortened, *fit_params)
 
@@ -52,5 +53,5 @@ plt.legend(fontsize=14)
 plt.savefig('plots/D_rot_sampled.pdf')
 
 with open('data/D_rot_value.txt', 'w') as file:
-    file.write('# D\n')
+    file.write('# D_rot (directly measured)\n')
     file.write('{}\n'.format(fit_params[0]/(2)))
